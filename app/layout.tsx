2fbf2ad69f2e6d58d618f85389f5bf5a015d3b9e
Saad <126159578+saadfalcon1@users.noreply.github.com>
2025-11-03 16:55:04 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Ahbor reyting',
+  description: 'Banklarning YouTubedagi faoliyati va ko‘rsatkichlari',
   generator: 'v0.app',
 }
 
